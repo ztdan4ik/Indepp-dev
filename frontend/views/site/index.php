f82b4aac556866f62f0f9a58cfdc0e28ef9dbe18
ztdan4ik <krajduba@gmail.com>
2015-11-07 16:07:01 +0200

    07.11.2015

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,5 +15,6 @@ $this->title = 'Indepp Team';
         <li><a href="<?=Url::toRoute('/site/about');?>">О нас</a></li>
         <li><a href="<?=Url::toRoute('/site/brief');?>">Заполнить Бриф</a></li>
         <?php /* <li><a href="<?=Url::toRoute('/site/partner');?>">Партнерка</a></li> */ ?>
+        <li><a href="<?=Url::toRoute('/site/test');?>">Тест</a></li>
     </ul>
 </div>
